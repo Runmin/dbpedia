ffb07f4b88a4cf3bf6d17d6114c1de3632757d04
Runmin Wang <runminwang@sylvester.cs.ucsb.edu>
2014-12-10 20:34:40 -0800

    dbpedia works, returns useful info. Need to make overlapping better

--- search.py
+++ search.py
@@ -1,5 +1,90 @@
 import urllib
 import re
+import HTMLParser
+import webbrowser
+import sys
+
+from urllib import quote_plus
+from httplib import HTTPConnection
+from getopt import getopt,GetoptError
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+entity_result = []
+
+class GoogleParser(HTMLParser.HTMLParser):
+    def __init__(self):
+        HTMLParser.HTMLParser.__init__(self)
+        self.handle_starttag = self.main_start
+        self.handle_data = self.main_data
+        self.handle_endtag = self.main_end
+
+    def main_start(self, tag, attrs):
+        if tag == "li" and len(attrs) > 0 and attrs[0] == ("class", "g"):
+            self.title = ""
+            self.url   = ""
+            self.text  = ""
+            self.handle_starttag = self.li_start
+            self.handle_data = self.li_data
+            self.handle_endtag = self.li_end
+
+    def main_data(self, data):
+        pass
+
+    def main_end(self, tag):
+        pass
+    # <li class="g"> ... </li>
+
+    def li_start(self, tag, attrs):
+        if tag == "h3":
+            self.handle_starttag = self.h3_start
+            self.handle_data = self.h3_data
+            self.handle_endtag = self.h3_end
+        elif tag == "div":
+            self.handle_starttag = self.div_start
+            self.handle_data = self.div_data
+            self.handle_endtag = self.div_end
+
+    def li_data(self, data):
+        pass
+
+    def li_end(self, tag):
+        if tag == "div":
+            parse_google_entry(self.title, self.url, self.text)
+            self.handle_starttag = self.main_start
+            self.handle_data = self.main_data
+            self.handle_endtag = self.main_end
+    # <h3> ... </h3>
+
+    def h3_start(self, tag, attrs):
+        if tag == "a":
+            self.url = attrs[0][1]
+
+    def h3_data(self, data):
+        self.title += data
+
+    def h3_end(self, tag):
+        if tag == "h3": 
+            self.handle_starttag = self.li_start
+            self.handle_data = self.li_data
+            self.handle_endtag = self.li_end
+    # <div> ... </div>
+
+    def div_start(self, tag, start):
+        if tag == "br":
+            self.handle_starttag = self.li_start
+            self.handle_data = self.li_data
+            self.handle_endtag = self.li_end
+
+    def div_data(self, data):
+        self.text += data
+
+    def div_end(self, tag):
+        pass
+
+def parse_google_entry(title, url, text):
+    global entity_result
+    entity_result.append(title)
 
 def change_initial_to_upper(key_word): #key_word is a string
     key_list = key_word.split(' ')
@@ -15,7 +100,11 @@ def change_all_to_upper(key_word):
 
 def get_first_sentence(sen):
     sen_list = sen.split('.')
-    return sen_list[0]
+    return_sen = ""
+    for s in sen_list:
+	if len(return_sen) < 15:
+		return_sen += s
+    return return_sen
 
 def rm_parenthesis(txt):
     new_txt = re.sub(r'\[[^()]*\]',"",txt)
@@ -41,10 +130,10 @@ def overlapping_word(abstract_hash):
 	    against = abstract_hash[key]
             if len(against)< 1:
 		continue
-            print "against is : " + against 
+            #print "against is : " + against 
             for word in word_list:
                 if word in against:
-			print "true" + word
+			#print "true" + word
 			save_list.append(word)
 	    word_list = save_list
     print "[overlapping_word] result"
@@ -92,7 +181,9 @@ def understand_list_word(list_key_word):
     #print_hash(abstract_hash)
     l = overlapping_word(abstract_hash)    
     l = rm_defined_word(l)
-    print " ".join(l) + " wikipedia"
+    l.insert(0,'wikipedia')
+    print " ".join(l)
+    return l
 
 def get_abstract(html):
     pattern = "<p>"
@@ -102,25 +193,32 @@ def get_abstract(html):
     result = result.replace("</p>","")
     return result    
 
-def do_get_selection_element_list(input_list,s_list,index,num_ele,total,result):
-    #print "index = " + str(index) + "num_ele "
-    if num_ele == total and index <= len(input_list):
-     #   print "found!!!"
-     #   print s_list
-        result.append(s_list)
-    elif index >=len(input_list):
-        return
-    else:
-    #    print "s_list with index = " + str(index)
-    #    print s_list
-        save1 = []
-        save2 = []
-        copy_list(s_list,save1)
-        copy_list(s_list,save2)
-        do_get_selection_element_list(input_list,save1,index+1, num_ele,total,result)
-        save2.append(input_list[index])
-        do_get_selection_element_list(input_list,save2,index+1,num_ele+1,total,result)
-        return
+
+def parse_entity_result():
+    global entity_result
+    for sen in entity_result:
+	s = re.sub(r' - Wiki.*',"",sen)
+        print s
+
+def change_into_google_string(string_list):
+    sen = "+".join(string_list)
+    return sen
+
+def search_google_and_return_title(string_list,length):
+    url = "/search?num=%s&q=" %(length)
+    #url = "/search?q="
+    #print url
+    search  = change_into_google_string(string_list)
+    url += search
+    print "url is " + url
+    conn = HTTPConnection("www.google.com")
+    conn.request("GET", url)
+    resp = conn.getresponse()  
+    content = resp.read()
+    parser = GoogleParser()
+    parser.feed(content)
+    return ""
+
 """
 key_word = "roger federer"
 t = change_initial_to_upper(key_word) #key_word is a string
@@ -130,12 +228,29 @@ html = "afdadsfasdf<p>roger federer</p>"
 c = get_abstract(html)
 print c
 """
-key_word_list = ["Ferrari","Aston Martin","Porsche"]
+if len(sys.argv) < 3:
+    print "Need a query length argument"
+    sys.exit()
+
+length = sys.argv[1]
+key_word_list = []
+
+for i in range(2,len(sys.argv)):
+    key_word_list.append(sys.argv[i])
+print key_word_list
+
+
 key_word_list = ["ACM","IEEE"]
-key_word_list = ["roger federer","rafael nadal", "novak djokovic"]
 
-key_word_list = ["ucla","ucsd","ucsb","ucb"]
-key_word_list = ["kobe byrant","allen iverson"]
 key_word_list = ["Tennis","Basketball","Soccer"]
-
-understand_list_word(key_word_list)
+key_word_list = ["Ferrari","Aston Martin"]
+key_word_list = ["kobe byrant","allen iverson"]
+key_word_list = ["ucla","ucsd","ucsb","ucb"]
+key_word_list = ["roger federer","rafael nadal"]
+key_word_list = ["Harvard University","Stanford University"]
+key_word_list = ["Bill Gates","Steven Jobs"]
+key_word_list = ["Barack Obama","George Washington"]
+key_word_list = ["Brad Pitt","Tom Hanks"]
+l = understand_list_word(key_word_list)
+search_google_and_return_title(l,length)
+parse_entity_result()
