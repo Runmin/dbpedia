6573eb7d1874a2210e31631c8d5cb6c1830b2c0b
Runmin Wang <runminwang@csil.cs.ucsb.edu>
2014-12-16 23:49:59 -0800

    working edition

--- search.py
+++ search.py
@@ -11,7 +11,7 @@ from getopt import getopt,GetoptError
 reload(sys)
 sys.setdefaultencoding("utf-8")
 entity_result = []
-
+abstract_list = []
 class GoogleParser(HTMLParser.HTMLParser):
     def __init__(self):
         HTMLParser.HTMLParser.__init__(self)
@@ -91,8 +91,10 @@ def change_initial_to_upper(key_word): #key_word is a string
     word_list = []
     for word in key_list:
         title_word = word.title()
+	#print word
         word_list.append(title_word)
     join_word = "_".join(word_list)
+    #print join_word
     return join_word
 
 def change_all_to_upper(key_word):
@@ -112,7 +114,7 @@ def rm_parenthesis(txt):
     return txt
 
 def rm_defined_pattern(txt):
-    txt = re.sub(r'no.|No.',"no#",txt)
+    txt = re.sub(r'no\.|No\.',"no#",txt)
     return txt
 	
 def overlapping_word(abstract_hash):
@@ -122,7 +124,7 @@ def overlapping_word(abstract_hash):
     for key in abstract_hash:
 	if count == 0:
 	    match = abstract_hash[key]
-            print "match is " + match
+            #print "match is " + match
             word_list = match.split(' ')
  	    count +=1
         else:
@@ -130,21 +132,47 @@ def overlapping_word(abstract_hash):
 	    against = abstract_hash[key]
             if len(against)< 1:
 		continue
-            print "against is : " + against
+            #print "against is : " + against
             against = against.split(' ') 
             for word in word_list:
                 if word in against:
 			#print "true" + word
 			save_list.append(word)
 	    word_list = save_list
-    print "[overlapping_word] result"
+    #print "[overlapping_word] result"
     return word_list
     
+def overlapping_abstract():
+    global abstract_list
+    count = 0
+    match =""
+    word_list = ""
+    #print "[overlapping_abstract]"
+    for key in abstract_list:
+	if count == 0:
+	    match = key
+            #print "match is " + match
+            word_list = match.split(' ')
+ 	    count +=1
+        else:
+            save_list = []
+	    against = key
+            if len(against)< 1:
+		continue
+            #print "against is : " + against
+            against = against.split(' ') 
+            for word in word_list:
+                if word in against:
+			#print "true" + word
+			save_list.append(word)
+	    word_list = save_list
+    return word_list
+
 def rm_defined_word(word_list):
     new_list = []
-    black_list = ['i','is','a','an','the']
+    black_list = ['-','was','or','that','with','to','it','in','its','is','a','an','the','and','by','as']
     for word in word_list:
-	if word not in black_list:
+	if word not in black_list and word not in new_list:
 		new_list.append(word)
     return new_list
 
@@ -156,9 +184,9 @@ def rm_last_s_and_space(sen):
     new_list = []
     word_list = sen.split(' ')
     for x in word_list:
-	if x == "":
+	if x == "" or len(x)<=2:
 		continue
-	elif x[-1] == "s":
+	elif x[-1] == "s" and x[-2] !='i':
 		s = x[:-1]
                 new_list.append(s)
         else:
@@ -175,6 +203,8 @@ def check_if_disambiguous(html):
 
 def parse_out_url(html):
     result = re.search(r'xmlns:dbpedia-owl="http:\/\/dbpedia.org\/ontology\/".*<small>',html)
+    if result == None:
+	return ""
     url = result.group(0)
     result = re.search(r'href=.*',url)
     url = result.group(0)
@@ -182,38 +212,46 @@ def parse_out_url(html):
     return url
 
 def go_to_top_link(html):
-    print "go to top link"
-    url =  parse_out_url(html)    
+    #print "go to top link"
+    url =  parse_out_url(html)
+    if url == "":
+	return ""    
     conn = urllib.urlopen(url)
     html = conn.read()
     abstract= parse_abstract(html)    
+    return abstract
 
 def parse_abstract(html):
-    
+    global abstract_list
+
     abstract = get_abstract(html)
-    print abstract
+#    print abstract
+    if abstract != "":
+   	 abstract_list.append(abstract.lower())
     succinct_abstract = rm_parenthesis(abstract)
     succinct_abstract = rm_defined_pattern(succinct_abstract)
     #print "[parse_dbpedia]" + succinct_abstract
     first_sentence = get_first_sentence(succinct_abstract)
     first_sentence = rm_coma(first_sentence)
     first_sentence = rm_last_s_and_space(first_sentence)
-    print "[parse_dbpedia]" + first_sentence
+    #print "[parse_dbpedia]" + first_sentence
     return first_sentence
 
 def parse_dbpedia(key_word):
+    print "key word is " + key_word
     url = "http://dbpedia.org/page/"
     url += key_word
-    print "[parse_dbpedia] URL:" + url
+    #print "[parse_dbpedia] URL:" + url
+    print key_word
     conn = urllib.urlopen(url)
     html = conn.read()
-    
+    html = html 
     if check_if_disambiguous(html):
 	sen = go_to_top_link(html)
-        return sen
+        return sen.lower()
     else:
         first_sentence = parse_abstract(html)
-        return first_sentence
+        return first_sentence.lower()
 
 def test_go_top_link():
     key_word = 'ACM'
@@ -227,16 +265,21 @@ def print_hash(hash_input):
 def understand_list_word(list_key_word):
     abstract_hash= {}
     for key_word in list_key_word:
-        search_word = change_initial_to_upper(key_word)
+	search_word = key_word
+	if '_' not in key_word:
+        	search_word = change_initial_to_upper(key_word)
 	result = parse_dbpedia(search_word)
 	#print "length is " + str(len(dum))
         if len(result) < 1:
-		print "Enter ALL Upper Case"
+		#print "Enter ALL Upper Case"
 		result = parse_dbpedia(key_word.upper())
         abstract_hash[key_word] = result
     #print_hash(abstract_hash)
     l = overlapping_word(abstract_hash)    
     l = rm_defined_word(l)
+    if (len(l) <=2):
+       l = overlapping_abstract()
+       l = rm_defined_word(l)
     l.insert(0,'wikipedia')
     print " ".join(l)
     return l
@@ -252,6 +295,7 @@ def get_abstract(html):
 
 def parse_entity_result():
     global entity_result
+    print "The result returned by google is:"
     for sen in entity_result:
 	s = re.sub(r' - Wiki.*',"",sen)
         print s
@@ -279,6 +323,7 @@ def search_google_and_return_title(string_list,length):
     parser.feed(content)
     return ""
 
+#t = change_initial_to_upper('Alien_(film)') #key_word is a string
 """
 key_word = "roger federer"
 t = change_initial_to_upper(key_word) #key_word is a string
